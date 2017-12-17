b5f9be77f1154c618f71b2b8fff6a075d6b9e4af
n0nvme <svmpl3nvm3@gmail.com>
2017-12-17 13:55:08 +0300

    Add files via upload

--- Main.py
+++ Main.py
@@ -20,7 +20,7 @@ longPoll_ts = longPoll['ts']
 print(longPoll)
 
 message_states = {}
-workers = []
+workers = [161911947]
 workers_answers = {}
 
 # main loop
@@ -34,36 +34,39 @@ while True:
     if len(messages['updates']) != 0:
         if messages['updates'][0][0] == 7: #new message
             message = messages['updates'][1][-2]
+            print(message, end=' ')
             message_sender = messages['updates'][0][1]
+            print(message_sender)
 
             # working with student
             if message_sender not in workers:
                 if message_states.setdefault(message_sender) == None:
                     message_states[message_sender] = 1
                     api.messages.send(user_id=str(message_sender), message='Привет, опиши свою проблему.')
-                    problem = ''
+
                 elif message_states[message_sender] == 1:
                     api.messages.send(user_id=str(message_sender), message='Где произошла проблема?')
                     message_states[message_sender] = 2
-                    location = ''
+                    problem = message
                 elif message_states[message_sender] == 2:
                     api.messages.send(user_id=str(message_sender), message='В скором времени проблема будет устранена.')
-                    problems.add_task(problem, location)
+                    location = message
+                    problems.add_task(problem, location) # Write task to DB
                     message_states[message_sender] = None
                     for i in workers:
                         message_states[i] = None
-                        mess = 'Появилась новая проблема' + problem + '\n' + location
+                        mess = 'Появилась новая проблема\n' + problem + '\n' + location
                         api.messages.send(user_id=str(i), message=mess)
                 else: api.messages.send(user_id=str(message_sender), message='техническая ошибка')
             # chat with worker
             else:
-                workers_answers.setdefault[message_sender] = message
+                workers_answers[message_sender] = message
 
                 # Return to main message
                 if message_states.setdefault(message_sender) is None:
                     message_states[message_sender] = 1
                     mess = 'Выберите действие:\n1. Посмотреть список доступных задач\n2. Показать список выполняемых вами задач'
-                    api.messages.send(user_id=str(i), message=mess)
+                    api.messages.send(user_id=message_sender, message=mess)
 
                 # Choice after main message
                 elif message_states.setdefault(message_sender) == 1:
@@ -72,36 +75,36 @@ while True:
                     if workers_answers[message_sender] == '1':
                         message_states[message_sender] = 2
                         mess = problems.get_all_free_tasks() + 'Чтобы начать выполнять задачу укажите ее номер'
-                        api.messages.send(user_id=str(i), message=mess)
+                        api.messages.send(user_id=message_sender, message=mess)
 
                     # Show tasks in work
                     elif workers_answers[message_sender] == '2':
                         message_states[message_sender] = 3
                         mess = 'На данный момент вы устраняете следующие проблемы:\n' + problems.get_workers_task(message_sender) + 'Чтобы изменить статус задачи укажите ее номер, чтобы выйти отправьте 0'
-                        api.messages.send(user_id=str(i), message=mess)
+                        api.messages.send(user_id=message_sender, message=mess)
 
                     else:
                         message_states[message_sender] = None
-                        api.messages.send(user_id=str(i), message='Неизвестный формат сообщения')
+                        api.messages.send(user_id=message_sender, message='Неизвестный формат сообщения')
 
                 # Choice after new tasks
                 elif message_states.setdefault(message_sender) == 2:
                     message_states[message_sender] = None
                     mess = 'Задача появится в списке выполняемых'
-                    api.messages.send(user_id=str(i), message=mess)
+                    api.messages.send(user_id=message_sender, message=mess)
 
                 # Show choice of tasks in work
                 elif message_states.setdefault(message_sender) == 3:
                     message_states[message_sender] = 4
                     mess = 'На данный момент вы устраняете следующие проблемы' + problems.get_workers_task(message_sender)
-                    api.messages.send(user_id=str(i), message=mess)
+                    api.messages.send(user_id=message_sender, message=mess)
 
                 # Choice of tasks in work
                 elif message_states.setdefault(message_sender) == 4:
                     if workers_answers[message_sender] == '0':
                         message_states[message_sender] = 1
                         mess = 'Выберите действие:\n1. Посмотреть список доступных задач\n2. Показать список выполняемых вами задач'
-                        api.messages.send(user_id=str(i), message=mess)
+                        api.messages.send(user_id=message_sender, message=mess)
                     elif workers_answers[message_sender] != '1':
                         message_states[message_sender] = 5
                         mess = 'Что вы хотите сделать?\n1. Отметить задачу как выполненную\n2. Отказаться от выполнения задачи'
@@ -111,9 +114,9 @@ while True:
                     message_states[message_sender] = None
                     if workers_answers[message_sender] == '1':
                         mess = 'Задача отмечена как выполненная'
-                        api.messages.send(user_id=str(i), message=mess)
+                        api.messages.send(user_id=message_sender, message=mess)
                     elif workers_answers[message_sender] == '2':
                         mess = 'Вы отказались от этой задачи'
-                        api.messages.send(user_id=str(i), message=mess)
+                        api.messages.send(user_id=message_sender, message=mess)
 
     else: continue
--- TasksDB.py
+++ TasksDB.py
@@ -3,12 +3,12 @@ import sqlite3
 class TasksDB:
 
     def _initialize(self):
-        conn = sqlite3.connect('tasks.db')
-        c = conn.cursor()
+        self._conn = sqlite3.connect('tasks.db')
+        c = self._conn.cursor()
         # create table
         c.execute('''CREATE TABLE tasks
-                         (task_id int, task_text text, task_location text, worker_id text, task_status int)''')
-        conn.commit()
+                         (task_id int, task_text text, task_location text, worker_id text, task_status int);''')
+        self._conn.commit()
         return c
 
     def __init__(self):
@@ -21,24 +21,34 @@ class TasksDB:
             self._cur = self._initialize()
 
     def _generate_task_id(self):
-        self._cur.execute('SELECT MAX(task_id) FROM tasks')
+        self._cur.execute('SELECT MAX(task_id) FROM tasks;')
+        self._conn.commit()
         return '1' # REPLACE!!!
 
     def add_task(self, task_text, task_location):
         self._cur.execute('''INSERT INTO tasks (task_id, task_text, task_location, worker_id, task_status) VALUES 
-                            (''' + self._generate_task_id() + ', ' + '''"''' + task_text + '''"''' + ', ' + '''"''' + task_location + '''"''' + ', ' + '0' + ', ' + '0' + ')')
+                            (''' + self._generate_task_id() + ', ' + '''"''' + task_text + '''"''' + ', ' + '''"''' + task_location + '''"''' + ', ' + '0' + ', ' + '0' + ');')
+        self._conn.commit()
+        print('Written to DB')
 
     def change_task_status(self, task_id, task_status):
         self._cur.execute('''
                             ''')
+        self._conn.commit()
+
 
     def _get_task(self, task_id):
         self._cur.execute('SELECT * FROM tasks WHERE id=' + str(task_id))
+        self._conn.commit()
+
 
     def get_workers_task(self, worker_id):  # Add formatting to string with \n!!!
-        self._cur.execute('SELECT * FROM tasks WHERE worker_id=' + worker_id)
+        self._cur.execute('SELECT * FROM tasks WHERE worker_id="' + str(worker_id) + '"')
+        self._conn.commit()
+
         return '1'
 
     def get_all_free_tasks(self):  # Add formatting to string with \n!!!
         self._cur.execute('SELECT * FROM tasks WHERE task_status=0')
+        self._conn.commit()
         return '1'
